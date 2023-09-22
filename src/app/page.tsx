67c3bbbc8cde3479e2499510893af7fa518490a8
william.schonarth <williams@microsys.inf.br>
2023-09-22 17:56:10 -0300

    fix: fixed config of prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="text-slate-900 p-8 bg-slate-50 h-screen flex flex-col items-center text-center dark:bg-slate-900 dark:text-slate-100">
+    <div className="flex h-screen flex-col items-center bg-slate-50 p-8 text-center text-slate-900 dark:bg-slate-900 dark:text-slate-100">
       <div className="max-w-2xl">
-        <h1 className="font-bold text-3xl sm:text-4xl lg:text-5xl">
+        <h1 className="text-3xl font-bold sm:text-4xl lg:text-5xl">
           The big text is below
         </h1>
         <p className="mt-4 dark:text-slate-400">
@@ -11,7 +11,7 @@ export default function Home() {
           accusamus ab vero inventore, possimus ipsum autem ut natus eos
           expedita perspiciatis.
         </p>
-        <button className="bg-sky-500 text-white dark:bg-sky-400 dark:text-sky-950 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
+        <button className="mt-4 rounded-md bg-sky-500 px-4 py-2 font-medium text-white enabled:hover:bg-sky-600 disabled:cursor-not-allowed disabled:opacity-60 dark:bg-sky-400 dark:text-sky-950">
           Sign in
         </button>
       </div>

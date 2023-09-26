c3b192bdff9bd1ec63dccbea710a2f8aea406341
william.schonarth <williams@microsys.inf.br>
2023-09-26 13:51:27 -0300

    feat: created form

---
+++ src/components/FileInput/index.tsx
@@ -0,0 +1,11 @@
+import { Root } from './Root'
+import { Trigger } from './Trigger'
+import { ImagePreview } from './ImagePreview'
+import { Control } from './Control'
+
+export const FileInput = {
+  Root,
+  Trigger,
+  ImagePreview,
+  Control,
+}
